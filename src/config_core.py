e535345ccdd9f23e29c26b9b5a998b92cc68af8f
Saurabh Gupta <saurab.gupta83@gmail.com>
2022-11-14 14:48:53 -0700

    pushing temp changes

---
+++ src/config_core.py
@@ -0,0 +1,7 @@
+gvTeams=['Account Servicing Viper','Account Servicing Dev X','Account Servicing Blaze 2.0']
+gvProjects=[52090,45125,45127]
+gvPath = '/Users/saurabh/OneDrive - DriveTime/Saurabh/Metrics/CServcTrackMetrics.xlsx'
+gvTPToken=""
+gvSheetNameStories='Stories'
+gvSheetNameReleases='Releases'
+
